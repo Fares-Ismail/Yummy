9dc2d13a37b9b12b444c20c927ad58d0ebb63434
Fares <faresismail408@gmail.com>
2025-11-22 16:32:29 +0200

    ubdate: make dark,light mode; use Js & CSS

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@ const rowData = document.getElementById("rowData");
 const searchContainer = document.getElementById("searchContainer");
 const openCloseBtn = document.getElementById("openCloseBtn");
 
+document.getElementById("them").onclick = () => {
+if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+} else {
+    document.body.classList.add("dark-mode");
+}
+};
+
+
+
+
+
 let submitBtn;
 
 document.addEventListener("DOMContentLoaded", async () => {
